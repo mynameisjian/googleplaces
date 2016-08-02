d4c5bbff32b8fa04a6736b1dd3e3256cd18f8147
jian <jianjian19841984@gmail.com>
2016-08-02 22:27:44 +1000

    Update to php 7 , type hint arguments instead of validation

--- src/AbstractPlace.php
+++ src/AbstractPlace.php
@@ -5,127 +5,65 @@ namespace JianHan\GooglePlaces;
 abstract class AbstractPlace
 {
 
-	const GOOGLE_PLACE_URL_PREFIX = 'https://maps.googleapis.com/maps/api/place/';
-
 	protected $key;	
 
 	protected $requestParameters;
-	
-	protected $outputFormat;	
 
-	protected $requestUrl;
-
-	public function __construct($key, $requestParameters = array(), $outputFormat = 'json')
+	public function __construct(string $key)
 	{
-
-		$this->validateString($key, 'Key');
-
-		$this->validateString($outputFormat, 'Output format');
-
-		if($outputFormat != 'json' && $outputFormat != 'xml')
-		{
-			throw new \InvalidArgumentException('Output format must be json or xml');
-		}
-
-		$this->outputFormat = $outputFormat;
-			
+		
 		$this->key = $key;
 
-		$this->requestParameters = $this->validateRequestParameters($requestParameters);
-
-		$this->autoGenerateRequestUrl();
 	}
 
-	protected abstract function validateRequiredParameters();	
-
-	protected function autoGenerateRequestUrl()
+	protected abstract function validateRequiredParameters();
+	
+	protected function getClassShortName() : string
 	{
 
 		$reflection = new \ReflectionClass(get_called_class());
 
-		$this->requestUrl = self::GOOGLE_PLACE_URL_PREFIX.strtolower($reflection->getShortName()).'/'.$this->outputFormat;
+		return $reflection->getShortName();
 
 	}
 
-	protected function validateString($string, $displayName)
-	{
-
-		if(!is_string($string) || trim($string) === '')
-		{
-			throw new \InvalidArgumentException("$displayName must be a string and must not be empty");
-		}
-
-	}	
-
-	protected function validateRequestParameters($requestParameters)
-	{
-
-		if(!is_array($requestParameters)) throw new \InvalidArgumentException("Request parameters must be an array");
-
-		foreach ($requestParameters as $key => $value) 
-		{
-
-			$this->validateString($value, 'All request parameter values');
-
-			$this->validateString($key, 'All request parameter keys');
-
-		}		
-
-		return $requestParameters;
-
-	}
-
-	public function getKey()
+	public function getKey() : string
 	{
 
 		return $this->key;
 
 	}
 
-	public function setKey($key)
+	public function setKey(string $key)
 	{
-
-		$this->validateString($key, 'Key');
 
 		$this->key = $key;
 
 	}
 
-	public function getRequestParameters()
+	public function getRequestParameters() : array
 	{
 
 		return $this->requestParameters;
 
 	}
 	
-	public function setRequestParameters(array $requestParameters = array())
+	public function setRequestParameters(array $requestParameters = array()) : array
 	{
-
-		$this->validateRequestParameters($requestParameters);
 
 		$this->requestParameters = $requestParameters;
 
 	}
 
-	public function setRequestParameter($parameterKey, $parameterValue)
+	public function setRequestParameter(string $parameterKey, string $parameterValue)
 	{
-
-		$this->validateString($parameterKey, 'Request parameter key');
-
-		$this->validateString($parameterValue, 'Request parameter value');
-
-		$parameterKey = (string)$parameterKey;
-
-		$parameterValue = (string)$parameterValue;
 
 		$this->requestParameters[$parameterKey] = $parameterValue;
 
 	}
 
-	public function getRequestParameter($parameterKey)
+	public function getRequestParameter($parameterKey) : string
 	{
-
-		$this->validateString($parameterKey, 'Request parameter key');
 
 		return $this->requestParameters[$parameterKey];
 
